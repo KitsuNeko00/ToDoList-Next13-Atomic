8d7861bbe2fb62428163faae5bb461a04336f3b2
KitsuNeko <dpldpl123456@naver.com>
2023-10-03 00:04:36 +0900

    [CREATE] json-server 설정

--- src/api/home/home.ts
+++ src/api/home/home.ts
@@ -1,1 +1,13 @@
+import axios, { AxiosResponse } from "axios";
 
+const addToDoApi = (): Promise<AxiosResponse> => {
+  try {
+    const response = axios.get("https://jsonplaceholder.typicode.com/todos/1");
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export { addToDoApi };
